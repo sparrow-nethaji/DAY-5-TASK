4cbd342432359541978659f3d26236bf18a14a0d
Nethaji <166787162+sparrow-nethaji@users.noreply.github.com>
2024-05-07 23:44:39 +0530

    Add files via upload
    DAY 5 TASK COMPLETE.

---
+++ 2 script.js
@@ -0,0 +1,56 @@
+
+
+let myResume ={
+    
+    name: "Nethaji.B",
+    age : "24",
+    gender:"male",
+    hobbies:["listen music, play chess, play vollyball, cooking"],
+    addres:{
+           no:"4/87",
+           street:"chinna street",
+           village:"thadukupettai",
+           madal:"nagari",
+           dist:"chittoor",
+           pincode:"517590",
+           state:"AP",
+           country:"india"
+
+    },
+    skills:["microbiology petriplate handling","gram staing"], 
+    education : {
+        school:"c.s.i high school",
+        std:     "10th",
+        persantage:"62%",
+        year:"2015",
+
+        school:"st.charles higher secondry school",
+        std:"12th",
+        persantage:"60%",
+        year:"2017",
+
+        undergraduated:"jeya college of arts and science",
+        course:"Bsc.biotechnollogy",
+        persantage:"60%",
+        year:"2020",
+
+        postgraduated:"Dr.MGR educational research instiute",
+        course:"Msc.biotechnology",
+        persantage:"75%",
+        year:"2023"
+
+    },
+    language:"tamil,telugu,english",
+
+}
+     
+
+      for(let key in myResume){
+        console.log(key ,myResume(key))
+      }
+
+      for (let value of Object.values(myResume)) {
+        console.log(value);
+      }
+      myResume.hobbies.forEach(hobby => console.log(hobby));
+      myResume.skills.forEach(skill => console.log(skill));
---
+++ REDME.md
@@ -0,0 +1,5 @@
+## DAY 5 TASK
+
+1. Create your own resume dala in JSON format
+
+2. For the above JSON, torate inver alt for loops (for, for in, fox of, forEach).
